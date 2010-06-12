9d9d2b713746c7258be1d9362d990dd058300634
Marcelo Silveira <grande@gmail.com>
2010-06-11 23:35:04 -0300

    another try

--- cpfcnpj.rb
+++ cpfcnpj.rb
@@ -1,3 +1,13 @@
+begin
+  # Require the preresolved locked set of gems.
+  require ::File.expand_path('../.bundle/environment', __FILE__)
+rescue LoadError
+  # Fallback on doing the resolve at runtime.
+  require "rubygems"
+  require "bundler"
+  Bundler.setup
+end
+
 # Gems requires
 require 'rubygems'
 require 'sinatra'
